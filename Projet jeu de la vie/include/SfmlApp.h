ca15cdbb6b9fb4479dbf79ff2843f021440bf721
youneslzz <156720244+youneslzz@users.noreply.github.com>
2025-12-05 17:40:23 +0100

    Define SfmlApp constructor and member variables
    Added constructor and member variables to SfmlApp class.

--- Projet jeu de la vie/include/SfmlApp.h
+++ Projet jeu de la vie/include/SfmlApp.h
@@ -1,5 +1,24 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#include "Game.h"
+#include "ConwayRule.h"
+#include "GridRenderer.h"
+
 class SfmlApp
 {
-};
+public:
+    // stepDelaySeconds a une valeur par défaut 
+    SfmlApp(int width, int height, float cellSize, float stepDelaySeconds = 0.05f);
 
+    void run();
+
+private:
+    sf::RenderWindow m_window;
+    Game            m_game;
+    GridRenderer    m_renderer;
+
+    float           m_stepDelay;   // intervalle entre deux itérations 
+};
