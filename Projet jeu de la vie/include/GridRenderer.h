c1165f084a414dd574f58312f9b7512cd51a64a1
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-05 17:26:34 +0100

    Update GridRenderer.h

--- Projet jeu de la vie/include/GridRenderer.h
+++ Projet jeu de la vie/include/GridRenderer.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
 class GridRenderer
 {
-};
+public:
+    GridRenderer(const Game& game, float cellSize);
 
+    void draw(sf::RenderWindow& window);
+
+private:
+    const Game& m_game;
+    float            m_cellSize;
+    sf::RectangleShape m_cellShape;
+};
