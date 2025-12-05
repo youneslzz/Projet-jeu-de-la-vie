16438ded5c66bbf433968861f605d37fe1eca5be
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-05 17:19:48 +0100

    Update AliveState.h

--- Projet jeu de la vie/include/AliveState.h
+++ Projet jeu de la vie/include/AliveState.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "CellState.h"
-class AliveState :
-    public CellState
-{
-};
 
+class AliveState : public CellState
+{
+public:
+    bool isAlive() const override { return true; }
+    char toChar() const override { return 'O'; }
+
+    void makeAlive(Cell& cell) override {}
+    void makeDead(Cell& cell) override;
+};
