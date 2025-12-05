2f06aa3a3d9bfb6ba68ea46c94bd77d939e05ff9
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-05 17:20:45 +0100

    Update CellRule.h

--- Projet jeu de la vie/include/CellRule.h
+++ Projet jeu de la vie/include/CellRule.h
@@ -1,5 +1,9 @@
 #pragma once
-class CellRule
-{
-};
 
+class CellRule {
+public:
+    virtual ~CellRule() = default;
+
+    // Retourne true si la cellule sera vivante au prochain tour
+    virtual bool willBeAlive(bool currentlyAlive, int aliveNeighbours) const = 0;
+};
