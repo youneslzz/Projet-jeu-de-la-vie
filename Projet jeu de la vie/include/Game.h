7b7bb94e9b4781354f5a5d43740b7c0bb70a8f3d
youneslzz <156720244+youneslzz@users.noreply.github.com>
2025-12-05 17:38:59 +0100

    Implement Game class with constructor and methods
    Added constructor and member functions to Game class.

--- Projet jeu de la vie/include/Game.h
+++ Projet jeu de la vie/include/Game.h
@@ -1,5 +1,27 @@
 #pragma once
+
+#include <memory>
+#include "Grid.h"
+#include "Rule.h"
+
 class Game
 {
-};
+public:
+    Game(int width, int height, std::unique_ptr<Rule> rule);
 
+    void clear();
+    void randomize();
+    void step();
+
+    const Grid& getGrid() const;
+    Grid& getGrid()
+    {
+        return m_grid;
+    }
+
+private:
+    int m_width;
+    int m_height;
+    Grid m_grid;
+    std::unique_ptr<Rule> m_rule;
+};
