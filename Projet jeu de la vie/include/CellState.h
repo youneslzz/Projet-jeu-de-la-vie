81c07baaac690fe6039935baf1cdd27246a80e57
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-05 17:21:49 +0100

    Update CellState.h

--- Projet jeu de la vie/include/CellState.h
+++ Projet jeu de la vie/include/CellState.h
@@ -1,5 +1,16 @@
 #pragma once
+
+class Cell; // forward declaration
+
 class CellState
 {
-};
+public:
+    virtual ~CellState() = default;
 
+    virtual bool isAlive() const = 0;
+    virtual char toChar() const = 0;
+    virtual bool isObstacle() const { return false; }
+
+    virtual void makeAlive(Cell& cell) = 0;
+    virtual void makeDead(Cell& cell) = 0;
+};
