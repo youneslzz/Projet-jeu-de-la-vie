4f2882da3dfdb0b36aa565f87cb409d38ed55a73
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-05 17:22:46 +0100

    Update ConsoleRunner.h

--- Projet jeu de la vie/include/ConsoleRunner.h
+++ Projet jeu de la vie/include/ConsoleRunner.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include "Game.h"
+#include "ConsoleRenderer.h"
+#include "InputHandler.h"
+
 class ConsoleRunner
 {
-};
+public:
+    ConsoleRunner(int width, int height);
+    void run();
 
+private:
+    Game m_game;
+    ConsoleRenderer m_renderer;
+    InputHandler m_input;
+};
