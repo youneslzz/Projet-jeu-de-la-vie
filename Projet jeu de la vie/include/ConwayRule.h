3777ef629263d9034cab739e3cb58ad25136e497
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-05 17:24:33 +0100

    Update ConwayRule.h

--- Projet jeu de la vie/include/ConwayRule.h
+++ Projet jeu de la vie/include/ConwayRule.h
@@ -1,7 +1,14 @@
 #pragma once
-#include "CellRule.h"
-class ConwayRule :
-    public CellRule
-{
-};
 
+#include "Rule.h"
+#include "Grid.h"
+
+class ConwayRule : public Rule
+{
+public:
+    // Règle de Conway : retourne vrai si la cellule sera vivante
+    bool apply(bool alive, int neighbours) const override;
+
+    // Compte les voisins vivants
+    int countAliveNeighbours(const Grid& grid, int x, int y) const override;
+};
