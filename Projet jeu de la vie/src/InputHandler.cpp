72e69727bb4383e45cbaaae5396140144425afb7
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-06 03:52:16 +0100

    Update InputHandler.cpp

--- Projet jeu de la vie/src/InputHandler.cpp
+++ Projet jeu de la vie/src/InputHandler.cpp
@@ -1,1 +1,10 @@
 #include "InputHandler.h"
+#include <iostream>
+
+char InputHandler::getInput() const
+{
+    std::cout << "\nCommands: (n)ext | (r)andom | (c)lear | (q)uit\n> ";
+    char c;
+    std::cin >> c;
+    return c;
+}
