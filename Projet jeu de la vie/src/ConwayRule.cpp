6746993f2b80f86c1e4f8ec24cd36ba3452a2cdd
zianeneilv2-rgb <zianeneilv2@gmail.com>
2025-12-06 03:50:40 +0100

    Update ConwayRule.cpp

--- Projet jeu de la vie/src/ConwayRule.cpp
+++ Projet jeu de la vie/src/ConwayRule.cpp
@@ -1,1 +1,20 @@
 #include "ConwayRule.h"
+
+// Ici on réutilise directement le calcul déjà codé dans Grid
+int ConwayRule::countAliveNeighbours(const Grid& grid, int x, int y) const
+{
+    return grid.countAliveNeighbours(x, y);
+}
+
+bool ConwayRule::apply(bool alive, int neighbours) const
+{
+    // Règles de Conway :
+    // 1. Une cellule vivante avec 2 ou 3 voisins reste vivante.
+    // 2. Une cellule morte avec exactement 3 voisins devient vivante.
+    // 3. Sinon : morte.
+
+    if (alive)
+        return (neighbours == 2 || neighbours == 3);
+    else
+        return (neighbours == 3);
+}
